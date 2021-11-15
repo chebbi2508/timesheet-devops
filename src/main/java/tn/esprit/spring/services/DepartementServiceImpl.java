63126bf12e90b33d634ee01c8a860629c65ef84b
chebbi2508 <44964475+chebbi2508@users.noreply.github.com>
2021-11-15 03:48:11 +0100

    Update DepartementServiceImpl.java

--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -22,7 +22,7 @@ public class DepartementServiceImpl implements IDepartementService {
 	public List<Departement> retrieveAllDepartements() { 
 		List<Departement> departements = null; 
 		try {
-			
+			l.info("In method test aaaaaaa :"); 
 			 l.info("In method test aaaaaaa :"); 
 			l.info("In method test aaaaaaa :"); 
 			l.info("In method retrieveAllDepartements :"); 
